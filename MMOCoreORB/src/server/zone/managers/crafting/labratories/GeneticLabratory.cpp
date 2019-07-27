5b939be7c336b479e9a52bc7811b368f5dff42d9
Scott Kansas <spkansas@msn.com>
2019-07-27 14:00:18 -0600

    BH: Permit creatures to inherit AOE specials from DNA, and adjust calculated creature level.  Adjust Kinetic, Energy, and Saber maximum resists.

--- MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
+++ MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
@@ -44,8 +44,9 @@ String GeneticLabratory::pickSpecialAttack(String a, String b, String c, String 
 	} else {
 		effectiveSpecial = a;
 	}
-	if (effectiveSpecial.contains("creature"))
-		effectiveSpecial = "defaultattack";
+//	Allow creatures to inherit AOE specials from DNA
+/*	if (effectiveSpecial.contains("creature"))
+		effectiveSpecial = "defaultattack";  */
 	int roll = System::random(750);
 	// roll now determined by template quality
 	// we roll 0-800 if that number is < quality * 100 i.e. VHQ 100 VLQ 700 if we get less than the odds we dont stick the special
@@ -185,19 +186,19 @@ void GeneticLabratory::setInitialCraftingValues(TangibleObject* prototype, Manuf
 	// 1 percent: (1000 - 0) / 100.0f;
 	float blastMax, energyMax, kineticMax,heatMax,coldMax,electricMax,acidMax,stunMax,saberMax;
 	blastMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::BLAST,100.0f);
-	kineticMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::KINETIC,60.0f);
-	energyMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::ENERGY,60.0f);
+	kineticMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::KINETIC,80.0f);
+	energyMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::ENERGY,80.0f);
 	heatMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::HEAT,100.0f);
 	coldMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::COLD,100.0f);
 	electricMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::ELECTRICITY,100.0f);
 	acidMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::ACID,100.0f);
 	stunMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::STUN,100.0f);
-	saberMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::LIGHTSABER,100.0f);
+	saberMax = Genetics::resistanceFormula(phy,pro,men,psy,agr,SharedWeaponObjectTemplate::LIGHTSABER,40.0f);
 
 	// lets clear the special bit if it moved to effective range.
 	if (saberMax == 0) {
 		spSaber = false;
-		saberMax = 100;
+		saberMax = 40;
 	}
 	if (blastMax == 0) {
 		spBlast = false;
@@ -205,11 +206,11 @@ void GeneticLabratory::setInitialCraftingValues(TangibleObject* prototype, Manuf
 	}
 	if (kineticMax == 0) {
 		spKinetic = false;
-		kineticMax = 60;
+		kineticMax = 80;
 	}
 	if (energyMax == 0) {
 		spEnergy = false;
-		energyMax = 60;
+		energyMax = 80;
 	}
 	if (heatMax == 0) {
 		spHeat = false;
