d17fa282c91d836f69a0cf55b48091b0b99fb296
Scott Kansas <spkansas@msn.com>
2019-07-27 19:11:13 -0600

    Structure: Use both player and player bank funds to uncondem structure

--- MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructurePayUncondemnMaintenanceSuiCallback.h
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/StructurePayUncondemnMaintenanceSuiCallback.h
@@ -38,7 +38,7 @@ public:
 
 		int uncondemnCost = -structure->getSurplusMaintenance();
 
-		if (uncondemnCost < 0 || (creature->getBankCredits() < uncondemnCost)) {
+		if (uncondemnCost < 0 || (creature->getTotalCredits() < uncondemnCost)) {
 			StringIdChatParameter params("@player_structure:structure_condemned_owner_no_credits"); // "This structure has been condemned by the order of the Empire. It currently requires %DI credits to uncondemn this structure. You do not have sufficient funds in your bank account. Add sufficient funds to your account and return to regain access to this structure."
 			params.setDI(uncondemnCost);
 			creature->sendSystemMessage(params);
